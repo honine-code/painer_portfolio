21930153a5924f063e8ff72354908e21ce9eea1e
painer <lhidns@gmail.com>
2023-01-21 14:46:34 +0900

    20230121-1 commit

---
+++ src/main/java/com/painer/PainerApplication.java
@@ -0,0 +1,13 @@
+package com.painer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/painer/PainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.painer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
